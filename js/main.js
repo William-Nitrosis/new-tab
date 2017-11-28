61868373a9ad7637389f154201897d9eb0ac74b2
WilliamNua <William.Sparkes@student.nua.ac.uk>
2017-11-28 20:38:46 +0000

    More buttons and grid layout

--- js/main.js
+++ js/main.js
@@ -48,6 +48,37 @@ $(document).ready(function() {
     loadWeather('England',''); //@params location, woeid
 });
 
+
+// Count down
+var countDownDate = new Date("Jan 12, 2018").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("countdownDisplay").innerHTML = days + " Days ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
 function loadWeather(location, woeid) {
     $.simpleWeather({
         location: location,
@@ -90,6 +121,8 @@ startTime();
 function loadPage(page) {
     $("#index").css("display", "none");
     $("#email").css("display", "none");
+    $("#countdown").css("display", "none");
+    $("#notes").css("display", "none");
 
     switch(page) {
         case index:
@@ -97,6 +130,12 @@ function loadPage(page) {
             break;
         case email:
             $("#email").css("display", "inherit");
+            break;
+        case countdown:
+            $("#countdown").css("display", "inherit");
+            break;
+        case notes:
+            $("#notes").css("display", "inherit");
             break;
     }
 }
